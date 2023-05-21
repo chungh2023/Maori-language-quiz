168606351cfa0fc41495a8fbccd8b780ec047536
chungh2023 <126839629+chungh2023@users.noreply.github.com>
2023-05-21 19:07:24 +0900

    Create 02_ask_name_age_v1.py

---
+++ 02_ask_name_age_v1.py
@@ -0,0 +1,11 @@
+import random
+
+
+def get_name():
+    name_local = input("what is your name: ")
+    return name_local
+
+
+def get_age():
+    age_local = input("How old are you: ")
+    return age_local
