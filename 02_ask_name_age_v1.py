e7f0f886c7d5915aa61c24aa732ec2ab2bfdc8cc
chungh2023 <126839629+chungh2023@users.noreply.github.com>
2023-05-21 20:28:33 +0900

    Update 02_ask_name_age_v1.py

--- 02_ask_name_age_v1.py
+++ 02_ask_name_age_v1.py
@@ -9,3 +9,10 @@ def get_name():
 def get_age():
     age_local = input("How old are you: ")
     return age_local
+
+
+name = get_name()  # 1st function
+age = get_age() # 2nd function
+
+print("This is a test about numbers pf Māori and their number abbreviations.\n"
+      "You will need to enter the correct abbreviation for each number\n")
